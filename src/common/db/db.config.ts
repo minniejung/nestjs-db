6eb580c76d83b3d44093172acd327f9b8376420e
minniejung <jungmignonne@gmail.com>
2025-05-29 11:19:22 +0900

    solved

--- src/common/db/db.config.ts
+++ src/common/db/db.config.ts
@@ -1,26 +1,26 @@
-import { ConfigService } from '@nestjs/config';
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigService } from "@nestjs/config";
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 
 export const dbConfig = (
   configService: ConfigService
 ): TypeOrmModuleOptions => {
   const config: TypeOrmModuleOptions = {
     // Todo: DB Config를 완성시켜주세요.
-    type: 'postgres',
-    host: '',
-    port: 0,
-    username: '',
-    password: '',
-    database: '',
+    type: "postgres",
+    host: configService.get<string>("DB_HOST"),
+    port: configService.get<number>("DB_PORT"),
+    username: configService.get<string>("DB_USERNAME"),
+    password: configService.get<string>("DB_PASSWORD"),
+    database: configService.get<string>("DB_DATABASE"),
     autoLoadEntities: true,
     synchronize: true,
   };
 
-  console.log('📦 DB 설정 정보:', {
+  console.log("📦 DB 설정 정보:", {
     host: config.host,
     port: config.port,
     username: config.username,
-    password: '****',
+    password: "****",
     database: config.database,
   });
 
--- src/modules/user/repository/user.repository.ts
+++ src/modules/user/repository/user.repository.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { User } from '../../../common/db/entities/user.entity';
-import { CreateUserDto } from '../dto/create-user.dto';
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { User } from "../../../common/db/entities/user.entity";
+import { CreateUserDto } from "../dto/create-user.dto";
 
 @Injectable()
 export class UserRepository {
@@ -13,27 +13,30 @@ export class UserRepository {
 
   async findOne(options): Promise<User | undefined> {
     // Todo: findOne은 어떤 조건이 들어올지 몰라도 where절에 options를 통해 데이터를 조회해야 합니다.
-    return undefined;
+    const user = await this.userRepository.findOne({ where: options });
+    return user ?? undefined;
   }
 
   async findAll(): Promise<User[]> {
     // Todo: findAll은 전체 데이터를 조회해야 합니다.
-    return [];
+    return this.userRepository.find();
   }
 
   async create(createUserDto: CreateUserDto): Promise<User> {
     // Todo: create은 유저 데이터를 생성해야 합니다.
-    return {} as User;
+    const user = this.userRepository.create(createUserDto);
+    return await this.userRepository.save(user);
   }
 
   async countAll(): Promise<number> {
     // Todo: countAll은 전체 데이터의 개수를 조회해야 합니다.
-    return 0;
+    return this.userRepository.count();
   }
 
   async existsByUserId(userId: string): Promise<boolean> {
     // Todo: existsByUserId은 userId로 유저의 정보가 있는지 확인해야 합니다. 리턴은 boolean으로 해주세요.
-    return false;
+    const count = await this.userRepository.count({ where: { userId } });
+    return count > 0;
   }
 
   async update(
@@ -41,10 +44,18 @@ export class UserRepository {
     updateUserDto: Partial<CreateUserDto>
   ): Promise<User> {
     // Todo: update은 userId로 유저의 정보를 수정해야 합니다.
-    return {} as User;
+    const existingUser = await this.findOne({ userId });
+
+    if (!existingUser) {
+      throw new NotFoundException(`User with ID ${userId} not found`);
+    }
+
+    const merged = this.userRepository.merge(existingUser, updateUserDto);
+    return await this.userRepository.save(merged);
   }
 
   async delete(userId: string): Promise<void> {
     // Todo: delete은 userId로 유저의 정보를 삭제해야 합니다.
+    await this.userRepository.delete({ userId });
   }
 }
